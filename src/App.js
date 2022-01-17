fef49bd17a85d37d69c9e2eeec0667383de64263
friendbro <devanshkhetwani@gmail.com>
2022-01-17 12:58:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ import LinksPage from "./pages/LinksPage";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" exact element={<Navigate to="/links" />} />
       <Route path="/categories" exact element={<CategoriesPage />} />
       <Route path="/categories/:id" exact element={<LinksPage />} />
       <Route path="/links" exact element={<AllLinksPage />} />
       <Route path="/links/:id" exact element={<LinkPage />} />
+      <Route path="*" exact element={<Navigate to="/links" />} />
     </Routes>
   );
 }
